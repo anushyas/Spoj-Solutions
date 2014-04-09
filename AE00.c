01f16c1d6f44d3633d7b290616957a8f3060c480
anushyas <anushya1995cool@gmail.com>
2014-04-09 09:18:14 +0530

    Create AE00.c

---
+++ AE00.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,j,n,cnt=0;
+    scanf("%d",&n);
+
+    //    counting no. of rectangles
+    for(i=1;i<=sqrt(n);i++)
+        for(j=i+1;i*j<=n;j++)
+            cnt++;
+
+    //    counting no. of squares
+    cnt+=sqrt(n);
+    printf("%d",cnt);
+    return 0;
+}
+
